2bf42adb5cc9fe04c90621b57d0d38796e420f82
FullsackDeveloper <xonimovoybek003@gmail.com>
2023-08-14 10:35:25 +0500

    yangi dars

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-console.log("salom dunyo");
+// console.log("salom dunyo");
 // alert('dastur toxtadi');
 // console.log('hello world');
-document.getElementById('h1').style.fontSize = '800px';
+// document.getElementById('h1').style.fontSize = '800px';
 
+// let result;
+// let numberA, numberB;
+// numberA = 40;
+// numberB = 60;
+// result = numberA + numberB;
+// console.log("yig'indi = " + result);
+// result = numberA - numberB;
+// console.log("ayirma = " + result);
+// result = numberA * numberB;
+// console.log("kopaytma = " + result);
+// result = numberA / numberB;
+// console.log("bo'linma = " + result);
+
+//  const birthday = '18.08.2003';
+// let ism = 'Oybek';
+// let familiya = 'Xonimov';
+// let yosh = '20';
+// console.log(ism);
+// console.log(familiya);
+// console.log(yosh);
---
+++ script_1.js
@@ -0,0 +1,11 @@
+// console.log("here");
+// let name = "Ilya";
+
+// alert( `hello ${1}` ); // ?
+
+// alert( `hello ${"name"}` ); // ?
+
+// alert( `hello ${name}` ); // ?
+
+let isGreater = 4 > 1;
+alert( isGreater )
