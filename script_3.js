fc30e906cd05f6b44d669a5fc07e00afc0c07859
FullsackDeveloper <xonimovoybek003@gmail.com>
2023-12-24 15:48:18 +0500

    Function ham object

---
+++ script_3.js
@@ -0,0 +1,30 @@
+// // Factory function 
+
+// function factor() {
+//     return{
+//         radius,
+//         draw(){
+//         console.log("doira");
+//         }
+//     }
+// }
+
+// // Constructor function
+
+// function Constructor(radius) {
+//     this.raduis = radius,
+//     this.draw = draw() {
+//         console.log("doira");
+//     }
+// }
+// const circle_1 =  factor(2);
+// const circle_2 = new Constructor(3);
+
+
+function Circle(){
+    this.radius = radius,
+    this.draw = function(){
+        console.log("doira");
+    }
+}
+console.log(Circle.name, Circle.length, Circle.);
