70933bbedcf0cfb8151e094821bc9e853e6017ac
FullsackDeveloper <xonimovoybek003@gmail.com>
2024-01-02 11:38:22 +0500

    Update script_3.js

--- script_3.js
+++ script_3.js
@@ -21,10 +21,59 @@
 // const circle_2 = new Constructor(3);
 
 
-function Circle(){
-    this.radius = radius,
-    this.draw = function(){
-        console.log("doira");
+// function Circle(radius){
+//     this.radius = radius,
+//     this.draw = function(){
+//         console.log("doira");
+//     }
+// }
+// console.log(Circle.name, Circle.length, Circle.arguments, Circle.caller, Circle.prototype);
+// console.log(Circle.apply, Circle.bind, Circle.call, Circle.toString);
+
+
+// Circle.prototype.displayInfo = function(){
+//     console.log(`This is a ${this.radius}.`);
+// }
+
+// let createName = new Circle("radius");
+// createName.displayInfo();
+
+//  apply, call, bind ==> Bu metodlar function ni chaqirishda ishlatiladi.
+
+//  apply function ning ishlatilishi:
+
+// function greet(message) {
+//     console.log(message + ' ' + this.name);
+// }
+
+// let person = { name: 'John' };
+
+// greet.apply(person, ['Hello']); //  Hello John
+
+//  bind metodining ishlatilishi:
+
+// function greet(massage) {    
+//     console.log(massage + " " + this.name);
+// }
+// let person = {name: 'John'};
+// let greetPerson = greet.bind(person);
+// greetPerson("Hello");
+
+//  call metodining ishlatilishi :
+
+// function greet(message) {
+//     console.log(message + ' ' + this.name);
+// }
+
+// let person = { name: 'John' };
+
+// greet.call(person, 'Hello'); // Output: Hello John
+
+
+// // // // // // // // // // // // // // // // // // // / / / //  //  //
+
+function Constructor(radius) {
+        this.raduis = radius,
+        console.log(Constructor.toString());
     }
-}
-console.log(Circle.name, Circle.length, Circle.);
+    const circle_2 = new Constructor(3);
